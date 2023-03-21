c353129b76802639f52615bb92c938b28185a25e
zxcola <zxcola1@gmail.com>
2023-03-21 19:21:20 +0300

    init commit

---
+++ src/main/java/com/CarSellingService/CarSellingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.CarSellingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSellingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSellingServiceApplication.class, args);
+	}
+
+}
