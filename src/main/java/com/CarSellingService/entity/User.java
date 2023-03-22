ba424716e2e78fcb5666ed2737004dfedb9b83fc
zxcola <zxcola1@gmail.com>
2023-03-22 20:00:27 +0300

    init commit

---
+++ src/main/java/com/CarSellingService/entity/User.java
@@ -0,0 +1,22 @@
+package com.CarSellingService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
