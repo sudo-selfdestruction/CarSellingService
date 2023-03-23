7c90bec715a9923ea2c5b422849e4c252953e98a
zxcola <zxcola1@gmail.com>
2023-03-23 17:59:48 +0300

    init commit

---
+++ src/main/java/com/CarSellingService/entity/Offer.java
@@ -0,0 +1,42 @@
+package com.CarSellingService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Offer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String carName;
+    private String description;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
---
+++ src/main/java/com/CarSellingService/repository/OfferRepository.java
@@ -0,0 +1,8 @@
+package com.CarSellingService.repository;
+
+import com.CarSellingService.entity.Offer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, Long> {
+
+}
