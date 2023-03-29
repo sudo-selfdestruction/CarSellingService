9f47f5bd0eff1666a20bc81e1490f38b23cfebfb
zxcola <zxcola1@gmail.com>
2023-03-29 10:11:22 +0300

    fix: repository

--- src/main/java/com/CarSellingService/repository/OfferRepository.java
+++ src/main/java/com/CarSellingService/repository/OfferRepository.java
@@ -2,7 +2,9 @@ package com.CarSellingService.repository;
 
 import com.CarSellingService.entity.Offer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OfferRepository extends CrudRepository<Offer, Long> {
 
 }
