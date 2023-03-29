5e797b5d1c4786a15e62dcc4613c31a6b9c76537
zxcola <zxcola1@gmail.com>
2023-03-29 10:10:29 +0300

    refactor: minor refactoring

--- src/main/java/com/CarSellingService/CarSellingServiceApplication.java
+++ src/main/java/com/CarSellingService/CarSellingServiceApplication.java
@@ -1,11 +1,20 @@
 package com.CarSellingService;
 
+import com.CarSellingService.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.CarSellingService.repository")
 public class CarSellingServiceApplication {
 
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	public static void main(String[] args) {
 		SpringApplication.run(CarSellingServiceApplication.class, args);
 	}
