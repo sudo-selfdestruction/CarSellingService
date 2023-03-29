4a4e977cb849f377a8416b73bbef04bebb6ea9d4
zxcola <zxcola1@gmail.com>
2023-03-29 10:12:30 +0300

    fix: repository

--- src/main/java/com/CarSellingService/repository/RoleRepository.java
+++ src/main/java/com/CarSellingService/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.CarSellingService.repository;
 
 import com.CarSellingService.entity.Roles;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends CrudRepository<Roles, Long> {
 }
