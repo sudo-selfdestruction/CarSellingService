5d1de7553ae89fd42537ce62623d2efb226a84c6
zxcola <zxcola1@gmail.com>
2023-03-29 10:19:24 +0300

    fix: repository

--- src/main/java/com/CarSellingService/repository/UserRepository.java
+++ src/main/java/com/CarSellingService/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.CarSellingService.repository;
 
 import com.CarSellingService.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
 }
