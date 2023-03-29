d72df18cce1b0ded61dead9fd490e3b2433e557c
zxcola <zxcola1@gmail.com>
2023-03-29 10:18:58 +0300

    add: minor changes

--- src/main/java/com/CarSellingService/entity/User.java
+++ src/main/java/com/CarSellingService/entity/User.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Entity
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 //    @Size(min=2, message = "Не меньше 5 знаков")
     private String username;
