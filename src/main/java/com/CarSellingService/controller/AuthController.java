2a4fdc6e8a0a6ad5231e4e2af2b4e43f12ad3a54
zxcola <zxcola1@gmail.com>
2023-04-12 18:16:26 +0300

    change: rebuilded to REST

--- src/main/java/com/CarSellingService/controller/AuthController.java
+++ src/main/java/com/CarSellingService/controller/AuthController.java
@@ -4,60 +4,42 @@ import com.CarSellingService.entity.User;
 import com.CarSellingService.repository.UserRepository;
 import com.CarSellingService.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
+import java.util.logging.Logger;
 
-@Controller
+
+@RestController
 public class AuthController {
-
+    private Logger logger = Logger.getLogger(AuthController.class.getName());
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
-    @GetMapping("/signUp.html")
-    public String signUp(Model model) {
-//        model.addAttribute("userForm", new User());
-        model.addAttribute("title", "Sign up to JDM");
-        return "signUp";
+    @GetMapping("/signUp")
+    public void blank(){
+
     }
-
-    @PostMapping("/signUp.html")
-    public String addUser(@ModelAttribute("userForm") User userForm,
-                          @RequestParam("username") String username,
-                          @RequestParam("password") String password,
-                          @RequestParam("passwordConfirm") String passwordConfirm,
-                          BindingResult bindingResult, Model model) {
-
-        System.out.println(username + " sad ");
-        userForm.setUsername(username);
-        userForm.setPassword(password);
-        userForm.setPasswordConfirm(passwordConfirm);
-
-        if (bindingResult.hasErrors()) {
-            return "signUp";
-        }
-        if (!Objects.equals(password, passwordConfirm)) {
-            model.addAttribute("passwordError", "Пароли не совпадают");
-            return "signUp";
-        }
+    @PostMapping("/signUp")
+    public User addUser(@RequestBody User userForm) {
 
         UserService userService = new UserService(userRepository);
 
         if (!userService.saveUser(userForm)) {
-            model.addAttribute("usernameError", "Пользователь с таким именем уже существует");
-            return "signUp";
+            logger.info("Пользователь с таким именем уже существует");
+            return null;
         }
+
+        if (!Objects.equals(userForm.getPassword(), userForm.getPasswordConfirm())) {
+            logger.info("Пароли не совпадают");
+            return null;
+        }
+
         userService.saveUser(userForm);
 
-        return "redirect:/";
+        return userForm;
     }
 }
