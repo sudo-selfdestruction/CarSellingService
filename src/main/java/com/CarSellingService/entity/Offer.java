c84eac8a336a495369591bbd103f4735980b6f07
zxcola <zxcola1@gmail.com>
2023-04-18 21:54:15 +0300

    refactor: minor refactoring

--- src/main/java/com/CarSellingService/entity/Offer.java
+++ src/main/java/com/CarSellingService/entity/Offer.java
@@ -1,7 +1,9 @@
 package com.CarSellingService.entity;
 
-import jakarta.persistence.*;
-import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Offer {
@@ -11,8 +13,6 @@ public class Offer {
     private Long id;
     private String carName;
     private String description;
-    @OneToMany
-    private List<User> user;
 
     public Offer() {
 
