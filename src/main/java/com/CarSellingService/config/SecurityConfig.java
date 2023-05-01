7ba872fabd52011dca665f199ecf45173cf9c718
zxcola <zxcola1@gmail.com>
2023-05-01 22:18:39 +0300

    refactor: minor

--- src/main/java/com/CarSellingService/config/SecurityConfig.java
+++ src/main/java/com/CarSellingService/config/SecurityConfig.java
@@ -1,50 +1,38 @@
 package com.CarSellingService.config;
 
 import com.CarSellingService.repository.UserRepository;
-import com.CarSellingService.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+    @Autowired
+    private UserRepository repository;
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    @Autowired
-    public UserDetailsService userDetailsService(UserRepository repository) {
-        return new UserService(repository);
-    }
+    CustomUserDetailService customUserDetailService;
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
              .csrf()
              .disable()
              .authorizeRequests()
-             .requestMatchers("signUp").not().fullyAuthenticated()
-             .requestMatchers("/admin/**").hasRole("ROLE_ADMIN")
-             .requestMatchers("/").permitAll()
-             .requestMatchers("/Offer/create").authenticated()
+             .requestMatchers("/signUp").not().fullyAuthenticated()
+             .requestMatchers("/logIn").permitAll()
+             .requestMatchers("/admin/**").hasRole("ADMIN")
+             .anyRequest().authenticated()
              .and()
-             //Настройка для входа в систему
-             .formLogin()
-//             .loginPage("/signUp")
-             //Перенарпавление на главную страницу после успешного входа
-             .defaultSuccessUrl("/")
-             .permitAll()
-             .and()
-             .logout()
-             .permitAll()
-             .logoutSuccessUrl("/");
+             .httpBasic();
         return http.build();
     }
 }
