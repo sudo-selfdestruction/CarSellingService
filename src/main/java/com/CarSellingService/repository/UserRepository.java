ef525da860b36b640b5de3fbb9c626444dd442c2
zxcola <zxcola1@gmail.com>
2023-05-03 16:04:42 +0300

    add: method to get authorized user

--- src/main/java/com/CarSellingService/repository/UserRepository.java
+++ src/main/java/com/CarSellingService/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.CarSellingService.repository;
 
 import com.CarSellingService.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM user WHERE username = :login")
+    User getUserByLogin(String login);
 
 }
